9500077caf3f77044785a03b4a966413c2ab2047
brtoone <brtoone@0536-83P45V1.ad.samford.edu>
2013-11-25 09:25:08 -0600

    updated db config to connect to cslinux

--- Config/database.php
+++ Config/database.php
@@ -4,9 +4,11 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'salvatoresdb',
+		'host' => 'cslinux.samford.edu',
 		'password' => 'wsMraUC7a3Ms8Xme',
+//		'host' => 'localhost',
+//		'password' => 'wsMraUC7a3Ms8Xme - your localhost password',
+		'login' => 'salvatoresdb',
 		'database' => 'salvatoresdb',
 	);
 }
