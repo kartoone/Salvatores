f36f3c252a6ab63ea78eef46c39fa801b1145587
brtoone <brtoone@0536-83P45V1.ad.samford.edu>
2013-11-25 08:43:52 -0600

    added route for pizzas menu

--- Config/routes.php
+++ Config/routes.php
@@ -28,6 +28,7 @@
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
 	Router::connect('/', array('controller' => 'menu_categories', 'action' => 'home'));
+	Router::connect('/pizzas', array('controller' => 'menu_categories', 'action' => 'menu', 1)); // 1 is the MenuCategory.id for the pizzas category
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
