3e02cdcac62df5cf925d988b4ac509c1c8fe280f
Bob <Bob@Bob-PC>
2014-01-02 13:19:55 -0600

    Added mobile menu view and methods in menu_categories controller.

--- Config/database.php
+++ Config/database.php
@@ -7,8 +7,7 @@ class DATABASE_CONFIG {
 //		'host' => 'cslinux.samford.edu',
 //		'password' => 'wsMraUC7a3Ms8Xme', // don't update this password ... this is the correct password for the cslinux db
 		'host' => 'localhost', // uncomment these two lines if you want to work with your local xampp database
-		'password' => 'rickgrimes',
-//'password' => 'wsMraUC7a3Ms8Xme', // update this password with your localhost db password
+'password' => 'wsMraUC7a3Ms8Xme', // update this password with your localhost db password
 		'login' => 'salvatoresdb',
 		'database' => 'salvatoresdb',
 	);
